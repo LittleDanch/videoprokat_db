bce635fc600eecc51dbd33c2d9fef3702fc9d02d
LittleDanch <danilalazarevrktmai@gmail.com>
2020-12-18 18:29:32 +0300

    Добавьте файлы проекта.

---
+++ videoprokat_db/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace videoprokat_db
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
